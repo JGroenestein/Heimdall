d94dbffa361a8fb0afd058719341078afcfc4a5f
JGroenestein <j.groenestein@gmail.com>
2013-05-21 00:31:39 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heimdall::Application.config.secret_token = '6aa8d3ba2a55ca9795aa752e7fea5b51864b5029e823233b5fd583277059cb0209a26a50eb6f0aba315d7244c0492f4175a959056b4b2299ae5a95227e4e8db1'
